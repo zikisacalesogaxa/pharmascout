181ae4bac9ca39a6d0947d79a400ebde6fc1466e
Zikisa Cale Sogaxa <zikisa@scout.tech>
2022-09-05 17:33:06 +0200

    :: setup everything i will need to build the FE

--- resources/js/Components/SidebarNav.vue
+++ resources/js/Components/SidebarNav.vue
@@ -1,5 +1,22 @@
 <script setup>
 import { Inertia } from '@inertiajs/inertia';
+const links = [
+	{
+		route: 'dashboard',
+		title: 'Dashboard',
+		value: 1,
+	},
+	{
+		route: 'profile.show',
+		title: 'Profile',
+		value: 2,
+	}
+	// {
+	// 	route: "teams.show, 1",
+	// 	title: 'Team',
+	// 	value: 3,
+	// }
+];
 const logout = () => {
 	Inertia.post(route('logout'));
 };
@@ -17,6 +34,17 @@ const logout = () => {
 		</v-sheet>
 
 		<v-list class="pt-0">
+			<div v-for="(link) in links">
+				<v-list-item :active="route().current(link.route)" :href="route(link.route)">
+					{{ link.title }}
+				</v-list-item>
+			</div>
+			<v-list-item @click="logout">
+				Logout
+			</v-list-item>
+		</v-list>
+
+		<!-- <v-list class="pt-0">
 			<v-list-item :active="route().current('dashboard')" :href="route('dashboard')">
 				Dashboard
 			</v-list-item>
@@ -25,18 +53,16 @@ const logout = () => {
 				Profile
 			</v-list-item>
 
-			<v-list-item :active="route().current('teams.show')" :href="route('teams.show', $page.props.user.current_team)">
-				{{ $page.props.user.current_team.name  }}
+			<v-list-item :active="route().current('teams.show')"
+				:href="route('teams.show', $page.props.user.current_team)">
+				{{ $page.props.user.current_team.name }}
 			</v-list-item>
-
-			<v-list-item @click="logout">
-				Logout
-			</v-list-item>
-		</v-list>
+		</v-list> -->
 
 		<!-- Sidebar footer -->
-		<div class="text-center bottom-0 bg-white absolute w-full">
-			<hr class="m-0">
+		<div class="text-center bottom-0 bg-white absolute w-full flex flex-col items-center">
+			<img src="../../images/pharmascoutlogo500.png" width="150" alt="Pharmascout Logo">
+			<hr class="m-0 w-full mt-2">
 			<p class="py-2 text-xs text-gray-700"><strong class="text-black-500 font-">Copyright</strong> Scout
 				Technologies © 2015-2022</p>
 		</div>
